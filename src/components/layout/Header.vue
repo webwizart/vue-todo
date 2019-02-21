890d3e74dd929d6240bcddfd6875b6eeb609e21c
w3bwizart.be@gmail.com <w3bwizart.be@gmail.com>
2019-02-21 23:17:50 +0100

    added vue logo

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,31 +1,37 @@
 <template>
-<header class="header">
-    <h1>TodoList</h1>
+  <header class="header">
+    <h1>
+      <img src="https://vuejs.org/images/logo.png" alt="vue.js logo">
+      TodoList
+    </h1>
 
     <nav id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </nav>
-</header>
+  </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
-}
+  name: "Header"
+};
 </script>
 
 <style scoped>
-    .header {
-        background-color: black;
-        color: #fff;
-        margin-top: 0;
-        text-align: center;
-        padding: 1%;
-    }
-    .header a {
-        color: #fff;
-        padding: 5px;
-        text-decoration: none;
-    }
+.header {
+  background-color: black;
+  color: #fff;
+  margin-top: 0;
+  text-align: center;
+  padding: 1%;
+}
+.header a {
+  color: #fff;
+  padding: 5px;
+  text-decoration: none;
+}
+header img {
+  width: 25px;
+}
 </style>
